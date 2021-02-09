57be2d392cfe0808d314e121bef882071e45c48e
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-02-09 16:36:51 -0500

    technical changes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@
 import './App.css';
 import React, {useState } from 'react';
 import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   const [position, setPosition] = useState({});
   
-  const positionInfo = (e) => {
-    e.preventDefault()
-    var c = document.getElementById(`${e.target.parentElement.id}`);
-    var element = document.getElementById(`${e.target.id}`);
-    var positionY = e.clientY - (c.offsetHeight / 2);
-    var positionX = e.clientX - (c.offsetWidth / 2);
+  // const positionInfo = (e) => {
+  //   e.preventDefault()
+  //   var c = document.getElementById(`${e.target.parentElement.id}`);
+  //   var element = document.getElementById(`${e.target.id}`);
+  //   var positionY = e.clientY - (c.offsetHeight / 2);
+  //   var positionX = e.clientX - (c.offsetWidth / 2);
    
-    var translateValue = "translate3d(" + positionX + "px," + positionY + "px, 0)";
-    element.style.transform = translateValue;
-  }
+  //   var translateValue = "translate3d(" + positionX + "px," + positionY + "px, 0)";
+  //   element.style.transform = translateValue;
+  // }
   return (
     <>
-      <About positionInfo={positionInfo} />
+      <About />
+      <Experience/>
+      <Projects/>
+      <Footer/>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,13 @@ import React from 'react';
 const About = (props) => {
     return (
         <div className="container" id="container">
-            <div className="item" id="element-1" onMouseDown={(e) => props.positionInfo(e) }>
+            
                 <h1 id='text'>About section</h1>
-            </div>
         </div>
     )
 }
 
 
 export default About
+
+{/* <div className="item" id="element-1" onMouseDown={(e) => props.positionInfo(e) }> */}
