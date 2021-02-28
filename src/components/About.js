7bf5bd975e64cfdf99c328b6cf518cef17cb7fcb
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-02-27 20:03:00 -0500

    name and navbar

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,9 @@ import React from 'react';
 const About = (props) => {
     return (
         <div className="container">
-            <h1 id='text'>Aleksandra Bystranowska Takahashi</h1>
+            <h1 id='text1'>Aleksandra </h1>
+            <h1 id='text2'>Bystranowska</h1>
+            <h1 id='text1'>Takahashi</h1>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Logo
+            <img src='images/mail.jpg' className='mail' alt='mail icon' />
         </div>
     )
 }
