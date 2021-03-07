9c301da3d50ea7afdef9340f4aa906e546745430
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-03-07 10:11:11 -0500

    structure changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,8 +3,33 @@ import React from 'react';
 const Experience = () => {
     return (
         <div>
-            Experience
-        </div>
+            <div className='company-name'>DoinGud</div>
+            <div className='position'>Engineering Lead</div>
+            <p className='dates'>05/2021 – actual</p>
+            <p className='description'>Coordinating communications between teams and processes to help sync development among engineers, product managers, and designers.
+                Owning development knowledge of Org Management tool features.
+            </p>
+            <div className='position'>Frontend Engineer</div>
+            <p className='dates'>01/2021 – actual</p>
+            <p className='description'>Collaborated with the engineering team to redesign the profile page for a more user-friendly experience.
+                An analyzed and updated database of the Fight Pandemics to improve resource search.
+             </p> 
+
+            <div className='company-name'> ND CHANNEL</div>
+            <p className='location'>New York City Metropolitan Area</p>
+            <div className='position'>Office Manager</div>
+            <p className='dates'>04/2019 – 03/2020</p>
+            <p className='description'>Created and implemented a prioritization and order status management system which helped improve the order status tracking and made customer communications smoother resulting in less mistakes.
+            Adapted customer service policy to make sure every customer was taken care of in an unconditional standard system, which in turn helped create an impeccable company reputation.
+               </p>  
+
+            <div className='company-name'>Fabryka Formy S.A.</div>
+            <p className='location'>Bytom, Śląskie, Poland</p>
+            <div className='position'>General Manager</div>
+            <p className='dates'>03/2016 – 11/2017</p>
+            <p className='description'>Accomplished department objectives by creating a timetable for all necessary documentation, which helped improve communication between organization and accounting stuff. Implemented “Name” policy towards customers to ameliorate friendly atmosphere which helped maintain membership roster despite the high prices.
+            </p>  
+    </div>
     );
 }
 
