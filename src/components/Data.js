4254025bf0b7d88a5947c575376c2608607edc92
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-03-19 10:01:29 -0400

    refaacturing data

---
+++ src/components/Data.js
@@ -0,0 +1,57 @@
+import ff from '../images/ff.jpeg';
+import fp from '../images/fp.png';
+import nd from '../images/nd.png';
+
+export const ExperienceData = [
+    {
+        name: "DoinGud",
+        position: "Engineering Lead",
+        dates: "03/2021 – actual",
+        location: "",
+        description: "Coordinating communications between teams and processes to help sync development among engineers, product managers, and designers. Owning development knowledge of Org Management tool features.",
+        image: {fp}
+    },
+    {
+        name: "DoinGud",
+        position: "Frontend Engineer",
+        dates: "01/2021 – actual",
+        location: "",
+        description: "Collaborated with the engineering team to redesign the profile page for a more user-friendly experience. An analyzed and updated database of the Fight Pandemics to improve resource search.",
+        image: {}
+    },
+    {
+        name: "ND CHANNEL",
+        position: "Office Manager",
+        dates: "04/2019 – 03/2020",
+        location: "New York City Metropolitan Area",
+        description: "Created and implemented a prioritization and order status management system which helped improve the order status tracking and made customer communications smoother resulting in less mistakes. Adapted customer service policy to make sure every customer was taken care of in an unconditional standard system, which in turn helped create an impeccable company reputation.",
+        image: {nd}
+    },
+    {
+        name: "Fabryka Formy S.A.",
+        position: "General Manager",
+        dates: "03/2016 – 11/2017",
+        location: "Bytom, Śląskie, Poland",
+        description: "Accomplished department objectives by creating a timetable for all necessary documentation, which helped improve communication between organization and accounting stuff. Implemented “Name” policy towards customers to ameliorate friendly atmosphere which helped maintain membership roster despite the high prices.",
+        image: {ff}
+    }
+];
+
+export const EducationData = [
+    {
+        name: "Flatiron School",
+        location: "New York, NY",
+        dates: "05/2020 - 08/2020",
+        profile: "Full Stack Web Development, Software Engineering "
+    },
+    {
+        name: "Upper Silesian Academy of Entrepreneurship",
+        location: "Chorzow, Poland",
+        dates: "09/2014 - 07/2017",
+        profile: "Modern Business - Management and Accounting (major in Organizational Psychology)"
+    }
+]
+	
+	
+
+ 	
--- src/components/Education.js
+++ src/components/Education.js
@@ -4,11 +4,7 @@ import React from 'react'
 const Education = () => {
     return (
         <div>
-            Flatiron School 	New York, NY
-            Full Stack Web Development, Software Engineering. 	05/2020 - 08/2020
-
-            Upper Silesian Academy of Entrepreneurship 	Chorzow, Poland
-            Modern Business - Management and Accounting (major in Organizational Psychology) 	09/2014 - 07/2017
+           
         </div>
     )
 }
