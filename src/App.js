e8ac6e36c198114140141d26765f8d7f244c264c
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-03-28 19:44:41 -0400

    redesign

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 import React, {useState } from 'react';
 import Home from './components/Home'
 import About from './components/About';
-import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import Education from './components/Education';
+import {PersonalData, ExperienceData, EducationData, ProjectsData} from './components/Data.js'
 
 function App() {
   const [position, setPosition] = useState({});
   return (
     <>
-      {/* <Home />
-      <About /> */}
-      {/* <Experience/> */}
-      <Projects/>
-      <Footer/>
+      <Home />
+      <About PersonalData={PersonalData}/>
+      <Projects ProjectsData={ProjectsData}/>
+      <Education EducationData={EducationData}/>
+      <Footer PersonalData={PersonalData}/>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,6 @@ const About = (props) => {
     return (
         <>
         <div className="container">
-            <h1 id='text1'>Aleksandra </h1>
-            <h1 id='text2'>Bystranowska</h1>
-            <h1 id='text1'>Takahashi</h1>
         </div>
         <div>
             <p>
