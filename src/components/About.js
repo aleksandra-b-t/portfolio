5ade542037b2ea16fd199063481d6c3163e3e3a3
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-03-28 20:04:04 -0400

    succesfull fetching data

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,8 @@ import React from 'react';
 const About = (props) => {
     return (
         <>
-        <div className="container">
-        </div>
-        <div>
-            <p>
-                 
-            </p>
-        </div>
-
+        <h1>Hello, {props.PersonalData[0].name}</h1>
+        {console.log(props)}
         </>
     )
 }
