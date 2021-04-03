04000811ec27b3e6a0e9e08b8d1b71f04e18e7e0
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-04-03 09:01:10 -0400

    education data

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 
-const Education = () => {
+const Education = (props) => {
+    const data = props.EducationData;
     return (
         <div>
-           
+           {data.map((school)=> 
+            <h1>{school.name}</h1>
+           )}
         </div>
     )
 }
