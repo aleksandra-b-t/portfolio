db18037ee040d9059e0192065979d7776cbfdb72
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2021-04-09 13:26:59 -0400

    carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Carousel, Item, Caption } from 'react-bootstrap';
+
 
 const Projects = (props) => {
     const data = props.ProjectsData;
     return (
     <>
-    {data.map((project) =>
-    <h2> {project.name} </h2>
-    )}
-
+        <Carousel fade> 
+        
+            {data.forEach((project) =>
+            <Carousel.Item>
+                {console.log(project)}
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt={project.name}
+                />
+                    <Carousel.Caption>
+                        <h2> {project.name} </h2>
+                    </Carousel.Caption>
+            </Carousel.Item>
+            )}
+        </Carousel>
     </>
     );
 }
