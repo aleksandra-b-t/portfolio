e74d7e9d3fe84e7de5b177ef1927e28a7d907c82
unknown <a.bystranowska@gmail.com>
2022-07-21 15:23:03 -0400

    css changes

--- portfolio/src/components/products/Products.jsx
+++ portfolio/src/components/products/Products.jsx
@@ -3,18 +3,9 @@ import './products.css'
 
 const Products = () => {
      return(
-         <div className="pl">
-             <div className="pl-text">
-                 <h1 className="pl-title">Create and inspire</h1>
-                 <p className="pl-desc">I am passionate about applying my nical skills to create impactful projects to make change. I continuously focus on self-improvement and pride myself in being agile, organized and thriving in a team environment.</p>
-             </div>
-             <div className="pl-list">
-                 <Product />
-                 <Product />
-                 <Product />
-                 <Product />
-                 <Product />
-             </div>
+         <div>
+            
+
          </div>
      )
 };
