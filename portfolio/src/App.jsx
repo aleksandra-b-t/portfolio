9f6ccf6cd0c46d0b5e46a50afb2f7515bcde9c16
Ola <a.bystranowska@gmail.com>
2022-10-03 11:34:45 -0400

    creating grid layout for navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
-import './app.css';
+import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Intro from './components/intro/Intro';
 import Products from './components/products/Products';
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar className="navBar" />
       <Intro className="intro"/>
       <About className="about"/>
       <Products className="products"/>
---
+++ portfolio/src/components/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import './navBar.css';
+
+
+function NavBar() {
+    return (
+        <div className="navBar">
+            <div className="logo">ABT</div>
+            <div className="home">HOME</div>
+            <div className="projects">PROJECTS</div>
+            <div className="services">SERVICES</div>
+            <div className="blog">BLOG</div>
+        </div>
+    );
+  }
+
+  export default NavBar;
