29dc385b3beb0929fc8a22dd2db6581772510d37
Ola <a.bystranowska@gmail.com>
2022-10-06 16:27:35 -0400

    changes to intro section

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -3,7 +3,9 @@ import "./intro.css"
 const Intro = () => {
   return (
     <div className="intro">
-      tutaj wrzucimy jakies intro, moze jakis zarcik, moze cos innego, poszukamy inspiracji online.
+      <div className="background"> na tlo
+        tutaj wrzucimy jakies intro, moze jakis zarcik, moze cos innego, poszukamy inspiracji online.
+      </div>
     </div>
   );
 }
